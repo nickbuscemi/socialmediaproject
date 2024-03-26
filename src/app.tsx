eda053f7e93ed0e0155650f20926914779437bbb
Nicholas Buscemi <nicholasbuscemi@Nicholass-MBP.fios-router.home>
2024-03-26 00:26:50 -0400

    sidebar ui complete

--- src/app.tsx
+++ src/app.tsx
@@ -4,8 +4,10 @@ import SigninForm from './_auth/forms/SigninForm';
 import SignupForm from './_auth/forms/SignupForm';
 import AuthLayout from './_auth/AuthLayout';
 import RootLayout from './_root/RootLayout';
-import { Home } from './_root/';
-import { Toaster } from "@/components/ui/toaster"
+import { Home } from './_root';
+import { Toaster } from "@/components/ui/toaster";
+import { Explore, Saved, AllUsers, CreatePost, EditPost, PostDetails, Profile, UpdateProfile } from '../src/_root/index';
+
 
 const app = () => {
   return (
@@ -20,7 +22,16 @@ const app = () => {
             {/* private routes */}
             <Route element={<RootLayout />}>
                 <Route index element={<Home />} />
+                <Route path='/explore' element={<Explore />} />
+                <Route path='/saved' element={<Saved />} />
+                <Route path='/all-users' element={<AllUsers />} />
+                <Route path='/create-post' element={<CreatePost />} />
+                <Route path='/update-post/:id' element={<EditPost />} />
+                <Route path='/posts/:id' element={<PostDetails />} />
+                <Route path='/profile/:id/*' element={<Profile />} />
+                <Route path='/update-profile/:id' element={<UpdateProfile />} />
             </Route>
+
         </Routes>
         <Toaster />
     </main>
